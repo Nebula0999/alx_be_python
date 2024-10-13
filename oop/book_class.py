16dde3a346e5f335c18cb000160fbf274e93e855
Allan <nabuleallan1@gmail.com>
2024-10-13 11:47:29 +0300

    made changes

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,8 +4,8 @@ class Book:
         self.author = str(author)
         self.year = int(year)
 
-    def __delattr__(self, name):
-        return f"Deleting {name.title}"
+    def __del__(self, name):
+        return f"Deleting {self.title}"
 
     def __str__(self, title, author, year):
         return f"{self.title} by {self.author}, published in {self.year}"
